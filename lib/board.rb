f0260269be6b693d035a83ee47ba5f447eeceda2
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 15:49:50 -0700

    Add cell_generator method to create a hash of cells inside a board

---
+++ lib/board.rb
@@ -0,0 +1,36 @@
+require "./lib/cell"
+
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = cell_generator
+  end
+
+  def cell_generator
+    cells = {}
+    coordinate_letters = Array("A".."D")
+    num = coordinate_letters.length
+    numbers =[]
+    num.times do
+      numbers << num
+      num -= 1
+    end
+    coordinates = []
+    coordinate_letters.each do |coordinate_letter|
+      numbers.sort.each do |number|
+        coordinate = coordinate_letter + number.to_s
+        coordinates << coordinate
+      end
+
+      coordinates
+    end
+
+    coordinates.each do |coordinate|
+      cells[coordinate] = Cell.new(coordinate)
+    end
+
+    cells
+  end
+
+end
