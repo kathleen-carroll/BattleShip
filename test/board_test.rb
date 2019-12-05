46aace5a211569a5280fa63750e7e6eb4840a2e4
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-04 18:19:16 -0700

    Create test to validate coordinates are on the board

--- test/board_test.rb
+++ test/board_test.rb
@@ -19,4 +19,12 @@ class BoardTest < Minitest::Test
   def test_board_has_cells
     assert_instance_of Hash, @board.cells
   end
+
+  def test_coordinates_are_on_board
+    assert_equal true, @board.validate_coordinate?("A1")
+    assert_equal true, @board.validate_coordinate?("D4")
+    assert_equal false, @board.validate_coordinate?("A5")
+    assert_equal false, @board.validate_coordinate?("E1")
+    assert_equal false, @board.validate_coordinate?("A22")
+  end
 end
