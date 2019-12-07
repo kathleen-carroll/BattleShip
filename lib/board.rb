8bba1044fc026e26a9956ba37853d57accfc89f5
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-07 15:11:34 -0700

    Update methods to return false values when the first comparison between coordinates is false. Add method to test for coordinate numbers that are all the same

--- lib/board.rb
+++ lib/board.rb
@@ -8,6 +8,8 @@ class Board
 
     @consecutive_numbers = false
     @consecutive_letters = false
+    @same_letters = false
+    @same_numbers = false
   end
 
   def cell_generator
@@ -43,6 +45,28 @@ class Board
     end
   end
 
+  def same_numbers?(ship, coordinates)
+    numbers = coordinates.map do |coordinate|
+      coordinate[-1] end.sort
+
+    counter = []
+    count = coordinates.length #- 1
+    count.times do
+      counter << count - 1
+      count -= 1
+      counter = counter.sort
+    end
+
+    counter.each do |count|
+      next_val = count + 1
+        return @same_numbers if next_val == coordinates.length
+        return @same_numbers = false if numbers[count].to_i + 1 != numbers[next_val].to_i
+        @same_numbers = true if numbers[count].to_i + 1 == numbers[next_val].to_i
+    end
+
+    @same_numbers
+  end
+
   def consecutive_numbers?(ship, coordinates)
     numbers = coordinates.map do |coordinate|
       coordinate[-1] end.sort
@@ -58,13 +82,13 @@ class Board
       counter.each do |count|
         next_val = count + 1
           return @consecutive_numbers if next_val == coordinates.length
+          return @consecutive_numbers = false if numbers[count].to_i + 1 != numbers[next_val].to_i
           @consecutive_numbers = true if numbers[count].to_i + 1 == numbers[next_val].to_i
-          @consecutive_numbers = false if numbers[count].to_i + 1 != numbers[next_val].to_i
       end
       @consecutive_numbers
   end
 
-  def consecutive_letters?(ship, coordinates)
+  def same_letters?(ship, coordinates)
     letters = coordinates.map do |coordinate|
       coordinate[0] end.sort
 
@@ -78,15 +102,34 @@ class Board
 
     counter.each do |count|
       next_val = count + 1
-        return @consecutive_letters if next_val == coordinates.length
-        @consecutive_letters = true if letters[count] == letters[next_val]
-          #require "pry"; binding.pry
-          #@consecutive_letters = true
-        #else @consecutive_letters = false
-        #end
-
-      #@consecutive_letters
+        return @same_letters if next_val == coordinates.length
+        return @same_letters = false if letters[count] != letters[next_val]
+        @same_letters = true if letters[count] == letters[next_val]
     end
+
+    @same_letters
+  end
+
+  def consecutive_letters?(ship, coordinates)
+    letters = coordinates.map do |coordinate|
+      coordinate[0].ord end.sort
+
+    counter = []
+    count = coordinates.length #- 1
+    count.times do
+      counter << count - 1
+      count -= 1
+      counter = counter.sort
+    end
+
+    counter.each do |count|
+      next_val = count + 1
+      #require "pry"; binding.pry
+        return @consecutive_letters if next_val == coordinates.length
+        return @consecutive_letters = false if letters[count] + 1 != letters[next_val]
+        @consecutive_letters = true if letters[count] + 1 == letters[next_val]
+    end
+
     @consecutive_letters
   end
 
