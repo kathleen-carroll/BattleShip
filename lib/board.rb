a9cac3498dbd92948b0ccf15d2d71ada5d8c64aa
Kathleen Carroll <khakicarroll@comcast.net>
2019-12-08 07:16:27 -0700

    Create place method to place ships on the board and pass test

--- lib/board.rb
+++ lib/board.rb
@@ -144,4 +144,11 @@ class Board
     end
   end
 
+  def place(ship, coordinates)
+    coordinates.each do |coordinate|
+      cell = cells[coordinate]
+      cell.place_ship(ship)
+    end
+  end
+
 end
