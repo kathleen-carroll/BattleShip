0ab0aea609aea32272f9077c90e9bf09bba554b5
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-09 15:51:36 -0700

    Fixed instance variable

--- lib/game.rb
+++ lib/game.rb
@@ -4,37 +4,28 @@ require './lib/cell'
 
 class Game
 
+  def initiliaze
+  @board = Board.new
+  end 
+
   def display_welcome_screen
     p "Welcome to BATTLESHIP.... GAMETIME!"
     p "Press (p)lay if you dare! \n read the (i)nstructions, or (q)uit?"
       input = gets.chomp.downcase
-    until input == "p" || input == "q"
       if input == "p"
       game_start
       elsif input == "q"
       puts "Bye!"
-      end
+
     end
   end
 
   def game_start
     p "I have laid out my ships on the grid."
     p "You now need to layout your two ships."
-    p "The first is #{@ship.length} long and the second is #{@ship.length}."
-    @board.render
-    p "Enter the squares for the #{@ship}:"
+    p "The Cruiser is three units long and the Submarine is two units long."
+    @board
+    p "Enter the squares for the cruiser:"
     @coordinates = gets.chomp
   end
-
-
-
-
-
-
-
-
-
-
-
-
 end
