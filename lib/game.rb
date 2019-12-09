a6c6d244ceb4d7e50b72aff624c88678512481f0
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-09 15:35:45 -0700

    made game_start method need to check on the variables

--- lib/game.rb
+++ lib/game.rb
@@ -3,4 +3,38 @@ require './lib/game'
 require './lib/cell'
 
 class Game
-end 
+
+  def display_welcome_screen
+    p "Welcome to BATTLESHIP.... GAMETIME!"
+    p "Press (p)lay if you dare! \n read the (i)nstructions, or (q)uit?"
+      input = gets.chomp.downcase
+    until input == "p" || input == "q"
+      if input == "p"
+      game_start
+      elsif input == "q"
+      puts "Bye!"
+      end
+    end
+  end
+
+  def game_start
+    p "I have laid out my ships on the grid."
+    p "You now need to layout your two ships."
+    p "The first is #{@ship.length} long and the second is #{@ship.length}."
+    @board.render
+    p "Enter the squares for the #{@ship}:"
+    @coordinates = gets.chomp
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
