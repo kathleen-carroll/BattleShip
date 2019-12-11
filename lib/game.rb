d08dd580cb0e28b73d60c7837a7812dd569790d5
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-10 22:02:21 -0700

    Added game_setup method

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,6 @@ require './lib/board'
 
 class Game
 
-
   def initialize
     @player_board = Board.new
     @cpu_board = Board.new
@@ -10,7 +9,7 @@ class Game
     @player_submarine = Ship.new("Submarine", 2)
   end
 
-  def display_welcome_screen
+  def welcome_screen
     p "Welcome to BATTLESHIP....GAMETIME!"
     p "Press (p)lay if you dare! or (q)uit?"
     selection_input = gets.chomp
@@ -25,4 +24,29 @@ class Game
       end
     end
   end
+
+  def game_setup
+    p "You now need to lay out your two ships."
+    p "The Cruiser is three units long and the Submarine is two units long."
+    puts @player_board.render
+    p "Enter the coordinates for the cruiser: Use the format A1 A2 A3 (3 spaces)"
+      loop do
+        ship = @player_cruiser
+        ship_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
+        if @player_board.valid_placement?(ship, ship_placement)
+          @player_board.place(@player_cruiser, ship_placement)
+          puts @player_board.render(true)
+      p "Now enter the coordinates for the submarine: Use the same format from above (2 spaces)"
+      ship = @player_submarine
+      ship_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
+      if @player_board.valid_placement?(ship, ship_placement)
+        @player_board.place(@player_submarine, ship_placement)
+        puts @player_board.render(true)
+        elsif @player_board.valid_placement?(ship, ship_placement) == false
+          puts "The coordinates you entered are invalid! Please try again."
+        end
+      end
+    end
+  end
+
 end
