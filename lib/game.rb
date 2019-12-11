7abbd98eaca7c378a8619d4a1c3b0ee6b8c456e6
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-10 21:53:00 -0700

    Created welcome_screen method line 19 - need to have game_setup working

--- lib/game.rb
+++ lib/game.rb
@@ -9,4 +9,20 @@ class Game
     @player_cruiser = Ship.new("Cruiser", 3)
     @player_submarine = Ship.new("Submarine", 2)
   end
+
+  def display_welcome_screen
+    p "Welcome to BATTLESHIP....GAMETIME!"
+    p "Press (p)lay if you dare! or (q)uit?"
+    selection_input = gets.chomp
+    until selection_input == "p" || selection_input == "q" do
+      if selection_input.downcase == "p"
+        #game_setup
+      elsif selection_input.downcase == "q"
+        p "Goodbye"
+      else
+        puts "Please enter p or q."
+        selection_input = gets.chomp
+      end
+    end
+  end
 end
