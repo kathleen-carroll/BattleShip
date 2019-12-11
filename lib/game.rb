401bea468dc8740bf4e40b8ba63033de0a0f474d
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-10 21:38:19 -0700

    created game file

--- lib/game.rb
+++ lib/game.rb
@@ -1,34 +1,6 @@
-require './lib/ship'
-require './lib/game'
-require './lib/cell'
+require './lib/board'
 
 class Game
 
-  # def initiliaze
-  # # @player_board = Board.new
-  # # @computer_board = Board.new
-  # # @ships = {
-  # #     cruiser: Ship.new("Cruiser", 3),
-  # #     submarine: Ship.new("Submarine", 2)
-  # end
 
-  # def display_welcome_screen
-  #   p "Welcome to BATTLESHIP.... GAMETIME!"
-  #   game_start
-  # end
-
-  def game_start
-    p "Press (p)lay if you dare! \n read the (i)nstructions, or (q)uit?"
-    selection_input = gets.chomp
-    until selection_input == "p" || selection_input == "q" do
-      if selection_input.downcase == "p"
-        setup #setup method below
-      elsif selection_input.downcase == "q"
-        abort "Goodbye"
-      else
-        puts "Please enter p or q."
-        selection_input = gets.chomp
-      end
-    end
-  end
 end
