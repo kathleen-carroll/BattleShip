1df34c457cb8ec4db49036fcf9449365d8e7a121
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-10 21:45:44 -0700

    created game test testing for board rendering

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,1 +1,17 @@
-display_welcome_screen
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/ship"
+require "./lib/cell"
+require "./lib/board"
+require "./lib/game"
+
+class GameTest < Minitest::Test
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @game.cpu_board
+  end
+end
