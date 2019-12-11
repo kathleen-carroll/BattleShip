adc4d5369a54a24da4fdcf29a00f64ad0e222814
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-10 21:50:39 -0700

    Creatd initialize method added instance variables

--- lib/game.rb
+++ lib/game.rb
@@ -3,4 +3,10 @@ require './lib/board'
 class Game
 
 
+  def initialize
+    @player_board = Board.new
+    @cpu_board = Board.new
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+  end
 end
